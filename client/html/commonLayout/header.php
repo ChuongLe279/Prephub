23cb00ae6ef731a03557d7fe2d0d9b0f1755bc5a
ChuongLe279 <lebuuchuong2024@gmail.com>
2026-04-02 19:56:48 +0700

    (feat): Add header and footer for HomePage

---
+++ client/html/commonLayout/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title>Document</title>
+</head>
+<style>
+
+</style>
+
+<body>
+  <div class="container">
+  <header class="d-flex flex-wrap align-items-center py-3 mb-4 border-bottom">
+    <div class="me-4">
+      <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+        <svg class="bi" width="40" height="32" role="img" aria-label="Bootstrap">
+          <use xlink:href="#bootstrap"></use>
+        </svg>
+      </a>
+    </div>
+
+    <ul class="nav me-auto mb-2 mb-md-0">
+      <li><a href="#" class="nav-link px-2 link-secondary">Trang chủ</a></li>
+      <li><a href="#" class="nav-link px-2">Đề thi online</a></li>
+      <li><a href="#" class="nav-link px-2">Liên hệ</a></li>
+      <li><a href="#" class="nav-link px-2">Tin tức</a></li>
+    </ul>
+
+    <div class="text-end">
+      <button type="button" class="btn btn-outline-primary me-2"><a href="/client/html/registerLogin.php">Đăng nhập</a></button>
+    </div>
+  </header>
+</div>
+</body>
+
+</html>
