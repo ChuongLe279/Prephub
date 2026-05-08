787b048276d848decacbfb985e775a7392aa2edb
verse91 <versedev.store@proton.me>
2026-05-08 14:42:11 +0700

    fix(score): cannot submit

--- server/controllers/score-controller.php
+++ server/controllers/score-controller.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../db/config.php';
 require_once __DIR__ . '/../models/attempt.php';
 
 if (session_status() === PHP_SESSION_NONE) {
-	session_start();
+    session_start();
 }
 
 header('Content-Type: application/json; charset=utf-8');
