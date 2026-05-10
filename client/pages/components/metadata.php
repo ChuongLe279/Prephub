452d47fecb261f887c0860707cd16ee93b3e4221
verse91 <versedev.store@proton.me>
2026-05-10 23:33:01 +0700

    refactor(homepage): seperate css into small files

--- client/pages/components/metadata.php
+++ client/pages/components/metadata.php
@@ -19,4 +19,4 @@
 
 <!-- font family -->
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=DM+Sans:wght@400;500;600;700&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="../styles/home.css">
+<link rel="stylesheet" href="../styles/homepage/main.css">
